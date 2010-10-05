df37700a0047042b39616ab6547fdfddece5f939
Andrea Censi <andrea@cds.caltech.edu>
2010-10-04 20:18:10 -0700

    Include static data

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='reprep',
+      version='0.9',
       package_dir={'':'src'},
-      py_modules=['reprep'],
-      install_requires=['numpy'] 
+      packages=find_packages(),
+      install_requires=['numpy'],
+      package_data={'': ['*.js','*.gif','*.jpg','*.cur','*.css']},
+      url='http://AndreaCensi.github.com/reprep/',
+      author='Andrea Censi',
+      author_email='andrea@cds.caltech.edu',
+      license="LGPL",
+      keywords="report reproducible research tables html latex",
 )
 
