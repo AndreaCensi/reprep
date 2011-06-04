ce850ce440528b36ff7a1b68348343218253e466
Andrea Censi <andrea@cds.caltech.edu>
2011-06-04 11:24:21 -0700

    changes to be removed

--- src/reprep/out/platex.py
+++ src/reprep/out/platex.py
@@ -90,7 +90,8 @@ class LatexEnvironment:
     
     def text(self, t):
         self.context.f.write(latexify(t))
-        
+    
+    #FIXME: make general!
     def tabular_simple(self, data, row_desc, col_desc, write_col_desc=True):
         ''' Writes a tabular environment with very simple options. '''
         def hline():
@@ -99,17 +100,17 @@ class LatexEnvironment:
             self.context.f.write(" & ".join(entries))
             self.context.f.write(' \\tabularnewline\n')
             
-        alignment_string = '|r||' + ('r|' * len(col_desc))
+        alignment_string = '|l||' + ('r|' * len(col_desc))
         self.context.f.write('\\begin{tabular}{%s}\n' % alignment_string)
         hline()
         if write_col_desc:
-            row_tex = [''] + ['\\makebox[0.7cm]{%s}' % x for x in col_desc]
+            row_tex = [''] + ['\\makebox[0.8cm]{%s}' % x for x in col_desc]
             write_row_tex(row_tex)
             hline()
             hline()
         
         for i, row in enumerate(row_desc):
-            row_tex = ['\\makebox[2.3cm][r]{%s}' % row] + ['\\makebox[0.7cm]{%s}' % x for x in data[i]]
+            row_tex = ['\\makebox[2.5cm][r]{%s}' % row] + ['\\makebox[0.8cm]{%s}' % x for x in data[i]]
             write_row_tex(row_tex)
             hline()
             
