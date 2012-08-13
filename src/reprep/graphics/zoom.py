3bfbc292a9da174176e8cbdc24789311f7c56d66
Andrea Censi <andrea@cds.caltech.edu>
2012-08-13 14:40:24 -0700

    minor bug

--- src/reprep/graphics/zoom.py
+++ src/reprep/graphics/zoom.py
@@ -1,10 +1,12 @@
 from . import contract, np
 
 
-@contract(M='array[HxWx3](uint8)', K='K,>1',
+@contract(M='array[HxWx3](uint8)', K='K,>=1',
           returns='array[(H*K)x(W*K)x3](uint8)')
 def rgb_zoom(M, K=10):
     ''' Zooms an RGB image by a fixed factor. '''
+    if K == 1:
+        return M.copy()
     H, W, _ = M.shape
     Z = np.zeros((H * K, W * K, 3), 'uint8')
     for i in range(3):
