506d66e9c2814eb17325c807d286a672db4b84da
Andrea Censi <andrea@cds.caltech.edu>
2012-08-23 23:30:25 -0700

    better implementation of frozendict; minor improvements to report

--- src/reprep/report_utils/store_results.py
+++ src/reprep/report_utils/store_results.py
@@ -1,3 +1,6 @@
+from reprep.report_utils.frozen import frozendict2
+
+frozendict = frozendict2
 
 class StoreResults(dict):
     def __setitem__(self, attrs, value):
@@ -28,16 +31,3 @@ class StoreResults(dict):
     def field(self, field):
         return (attrs[field] for attrs in self)  
 
-try:
-    frozenset
-except NameError:
-    from sets import ImmutableSet as frozenset
-
-class frozendict(dict):
-    __slots__ = ('_hash',)
-    
-    def __hash__(self):
-        rval = getattr(self, '_hash', None)
-        if rval is None:
-            rval = self._hash = hash(frozenset(self.iteritems()))
-        return rval
