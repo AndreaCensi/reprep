aadd02981afc2a39e9a25c833149e5e109b26808
Andrea Censi <acensi@ethz.ch>
2018-10-21 19:01:23 +0200

    fix

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup, find_packages
 
 
-
 def get_version(filename):
     import ast
     version = None
-    with file(filename) as f:
+    with open(filename) as f:
         for line in f:
             if line.startswith('__version__'):
                 version = ast.parse(line).body[0].value.s
@@ -16,8 +15,8 @@ def get_version(filename):
         raise ValueError(filename)
     return version
 
-version = get_version('src/reprep/__init__.py')
 
+version = get_version('src/reprep/__init__.py')
 
 scripts = [
     ('reprep_demos', 'reprep.demos.manager'),
@@ -26,18 +25,17 @@ scripts = [
 # this is the format for setuptools
 console_scripts = map(lambda s: '%s = %s:main' % (s[0], s[1]), scripts)
 
-package_data = {'':['*.*']}
-
+package_data = {'': ['*.*']}
 
 setup(name='reprep',
       version=version,
-      package_dir={'':'src'},
+      package_dir={'': 'src'},
       packages=find_packages('src'),
-	include_package_data=True,
+      include_package_data=True,
       install_requires=[
-            'docutils',
-            'PyContracts>=1.2,<2',
-            # 'PIL',
+          'docutils',
+          'PyContracts>=1.2,<2',
+          # 'PIL',
       ],
       package_data=package_data,
       url='http://AndreaCensi.github.com/reprep/',
@@ -47,6 +45,6 @@ setup(name='reprep',
       license="LGPL",
       keywords="report reproducible research tables html latex",
       download_url='http://github.com/AndreaCensi/reprep/tarball/%s' % version,
-      entry_points={ 'console_scripts': console_scripts},
-      zip_safe=False, # because of resources
-)
+      entry_points={'console_scripts': console_scripts},
+      zip_safe=False,  # because of resources
+      )
